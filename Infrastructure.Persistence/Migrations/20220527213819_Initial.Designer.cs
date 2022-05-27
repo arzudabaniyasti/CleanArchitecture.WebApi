641d84ec1d24e62eebb4dc5a438a90059e0e568f
Arzu <20180808032@ogr.akdeniz.edu.tr>
2022-05-27 23:53:35 +0300

    class3 addednew command

--- Infrastructure.Persistence/Migrations/20220527213819_Initial.Designer.cs
+++ Infrastructure.Persistence/Migrations/20220527213819_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220527130335_Initial")]
+    [Migration("20220527213819_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
